ffa14b91f7c9e372b2ac57b3c6c879afcbb6a728
Mahanidhi <mahanidhi.gk@gmail.com>
2025-10-13 10:21:56 +0530

    docs: datasets source links

--- webscraper.py
+++
@@ -1,9 +0,0 @@
-import requests
-import pandas as pd
-
-res = requests.get('https://gol.gg/game/stats/25989/page-fullstats/')
-
-df = pd.read_html(res.text, skiprows=[0])
-df = pd.concat(df)
-df.to_csv("data.csv", index=False)
-print(df)
